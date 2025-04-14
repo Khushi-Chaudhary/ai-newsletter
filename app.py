f8413661702f3f7388130b43a4494110c106ce9a
Khushi Chaudhary <95865646+Khushi-Chaudhary@users.noreply.github.com>
2025-04-14 17:49:41 +0530

    fixed app.py version 2

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ import streamlit as st
 from newsletter_generator import generate_newsletter
 from user_profiles import USER_PROFILES
 
+usernames = [profile["name"] for profile in USER_PROFILES] 
+
 st.set_page_config(page_title="AI Newsletter Generator", layout="centered")
 
 st.title("🧠 AI-Powered Personalized Newsletter Generator")
 
 # Dropdown for persona
-persona = st.selectbox("Choose a user persona:"), list
+persona = st.selectbox("Choose a user persona:",usernames)
